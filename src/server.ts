f974b126fb029bd52d29992544b211636b894230
Leonardo <leonardo.everton@inatel.br>
2025-03-23 12:23:31 -0300

    feat: create env file with zod config

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import {app} from "./app"
+import { ENV } from "./env"
 
 
 app.listen({
     host: '0.0.0.0',
-    port: 3333,
+    port: ENV.PORT,
 }).then(() =>{
     console.log("🚀 HTTP Server Running")
 })
