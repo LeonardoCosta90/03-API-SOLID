5620f55935a70606cbf222030f2416f899d5fb92
Leonardo <leonardo.everton@inatel.br>
2025-03-24 19:00:20 -0300

    test: tdd and mocking

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -3,6 +3,14 @@ import { Prisma, CheckIn } from '@prisma/client'
 import { randomUUID } from 'node:crypto'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findByUserIdOnDate(userId: string, date: Date) {
+    return this.checkIns.find(
+      (checkIn) =>
+        checkIn.user_id === userId && isSameDay(checkIn.created_at, date),
+    )
+  }
+
   public items: CheckIn[] = []
 
   async create(data: Prisma.CheckInUncheckedCreateInput) {
