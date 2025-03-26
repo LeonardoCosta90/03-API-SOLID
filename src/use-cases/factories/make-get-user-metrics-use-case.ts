d251359fe2fc9bdbab39f5001bc0c09c1769c24f
Leonardo <leonardo.everton@inatel.br>
2025-03-26 20:23:35 -0300

    feat: crete controllers checkins

--- src/use-cases/factories/make-get-user-metrics-use-case.ts
+++ src/use-cases/factories/make-get-user-metrics-use-case.ts
@@ -1,9 +1,9 @@
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { GetUserProfileUseCase } from '../get-user-profile'
+import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
+import { GetUserMetricsUseCase } from '../get-user-metrics'
 
-export function makeGetUserProfileUseCase() {
-  const usersRepository = new PrismaUsersRepository()
-  const useCase = new GetUserProfileUseCase(usersRepository)
+export function makeGetUserMetricsUseCase() {
+  const checkInsRepository = new PrismaCheckInsRepository()
+  const useCase = new GetUserMetricsUseCase(checkInsRepository)
 
   return useCase
 }
